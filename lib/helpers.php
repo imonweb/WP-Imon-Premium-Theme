f83d0e389f8b2363bcd774e3ca3d2e66f8a602d5
Imon <contact@imonweb.co.uk>
2022-05-29 23:49:27 +0100

    initial commit

--- lib/helpers.php
+++ lib/helpers.php
@@ -3,14 +3,14 @@
 
 function _themename_post_meta(){
   echo 'Posted on';
-  echo '<a href="' .  get_permalink()  . '">';
-  echo '<time datetime="' . get_the_date('c') . '">' . get_the_date('l, F j, Y') . '</time>';
+  echo '<a href="' .  esc_url(get_permalink())  . '">';
+  echo '<time datetime="' . esc_url(get_the_date('c')) . '">' . esc_html(get_the_date('l, F j, Y')) . '</time>';
   echo '</a>';
-  echo ' By <a href="' . get_author_posts_url( get_the_author_meta('ID')) . '">' . get_the_author() . '</a>';
+  echo ' By <a href="' . esc_url(get_author_posts_url( get_the_author_meta('ID'))) . '">' . esc_url(get_the_author()) . '</a>';
 }
 
 function _themename_readmore_link(){
-  echo '<a href="' . get_the_permalink() . '" title="' . the_title_attribute(['echo' => false]) . '">'; 
+  echo '<a href="' . esc_url(get_the_permalink()) . '" title="' . the_title_attribute(['echo' => false]) . '">'; 
   echo  'Read More <span class="u-screen-reader-text">About' . the_title() . '</span>';
   echo  '</a>';
 }
